2378fc03899ae760858b2d90d8d99aea2ed38583
Shrikant8705 <138581936+Shrikant8705@users.noreply.github.com>
2023-07-16 21:44:49 +0530

    Add files via upload

---
+++ Assignment11.py
@@ -0,0 +1,38 @@
+# Create a gui based form to take input from the user and then navigate to the particular site from 
+# where the user came to know about the content
+
+# for example:
+# create a form where the user is enquiring about the respective course
+# and in the form there is an option for asking where the user came to know about it, for eg: instagram ads 
+# or YouTube ads
+
+# and then when clicking the submit button
+# take the user to the faq page of that site
+
+from tkinter import *
+from webbrowser import *
+
+root = Tk(className= " Ads Survey")
+
+root.geometry("450x270")
+Label(root).pack()
+nameLabel = Label(root, text="Name:").pack()
+userName = Entry(root).pack()
+contactLabel = Label(root, text="Contact:").pack()
+userContact = Entry(root).pack()
+Label(root).pack()
+questionLabel = Label(master = root, text= "Where did you see this AD?", font=("Poppins medium",15)).pack()
+userChoice = StringVar(root)
+userChoice.set("Select One Option")
+checkBox = OptionMenu(root, userChoice, "FaceBook Ads", "Twitter Ads").pack()
+Label(root).pack()
+def submitOnClick():
+    if userChoice.get() == "Twitter Ads":
+        open("https://business.twitter.com/en/advertising.html")
+    elif userChoice.get() == "FaceBook Ads":
+        open("https://en-gb.facebook.com/business/ads")
+    root.destroy()
+submitButton = Button(root,text = "Submit",command = submitOnClick).pack()
+Label(root).pack()
+
+root.mainloop()
